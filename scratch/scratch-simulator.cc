8a62c2e7c378ba75eb7bd00f3d6cbc336e6830af
chi <1302656106@qq.com>
2024-03-08 15:56:45 +0000

    test github

--- scratch/scratch-simulator.cc
+++ scratch/scratch-simulator.cc
@@ -22,7 +22,7 @@ NS_LOG_COMPONENT_DEFINE("ScratchSimulator");
 int
 main(int argc, char* argv[])
 {
-    NS_LOG_UNCOND("Scratch Simulator test");
+    NS_LOG_UNCOND("Scratch Simulator");
 
     Simulator::Run();
     Simulator::Destroy();
